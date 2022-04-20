56dd887b6c3bee956505eb6b5eb6d385139d682f
CircleCI <no-reply@build.com>
2022-04-20 15:36:46 +0300

    fix

---
+++ scripts/bd_to_prod.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+
+setup_git() {
+    git config --global user.email "no-reply@build.com"
+    git config --global user.name "CircleCI"
+    git checkout master
+    # Avoid version failure
+    git stash
+}
+
+
+push_tags() {
+    git push origin master --tags
+}
+
+
+echo ".____                  .__                  .__        ";
+echo "|    |    __ __  _____ |__| ____   ____     |__| ____  ";
+echo "|    |   |  |  \/     \|  |/ ___\ /  _ \    |  |/  _ \ ";
+echo "|    |___|  |  /  Y Y  \  / /_/  >  <_> )   |  (  <_> )";
+echo "|_______ \____/|__|_|  /__\___  / \____/ /\ |__|\____/ ";
+echo "        \/           \/  /_____/         \/            ";
+echo
+echo "Deploy Python Tracer"
+
+setup_git
+
+
+echo "Done"
