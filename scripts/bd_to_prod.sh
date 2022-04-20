79f49aa61f6f17d80347631596cc9d43bb70efa9
CircleCI <no-reply@build.com>
2022-04-20 17:01:55 +0300

    adding twine

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -27,5 +27,17 @@ echo "Deploy Python Tracer"
 
 setup_git
 
+echo "Getting latest changes from git"
+changes=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline)
+
+sudo pip install --upgrade bumpversion
+bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
+
+push_tags
+
+echo "Uploading to PyPi"
+pip install twine wheel --quiet
+python setup.py bdist_wheel
+twine upload dist/*
 
 echo "Done"
