6c1033d9d20b2d7bfbb0dfa7a924959b3954879a
Michele Mancioppi <michelem@lumigo.io>
2022-05-26 13:13:34 +0200

    Remove unnecessary debug output about Python version

--- noxfile.py
+++ noxfile.py
@@ -26,8 +26,6 @@ def dependency_versions(dependency_name: str) -> List[str]:
 @nox.parametrize("uvicorn_version", dependency_versions("uvicorn"))
 @nox.parametrize("fastapi_version", dependency_versions("fastapi"))
 def integration_tests_fastapi(session, fastapi_version, uvicorn_version):
-    session.run("python3", "--version", external=True)
-
     try:
         session.install(f"uvicorn=={uvicorn_version}")
     except:  # noqa
