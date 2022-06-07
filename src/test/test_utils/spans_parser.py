0674a1fa4db21dc947dca3681437825baa5cf34a
CircleCI <no-reply@build.com>
2022-06-07 13:48:18 +0300

    foix

--- src/test/test_utils/spans_parser.py
+++ src/test/test_utils/spans_parser.py
@@ -33,23 +33,44 @@ class SpansContainer:
 
     @staticmethod
     def get_spans_from_file() -> SpansContainer:
-        return SpansContainer(spans=SpansContainer.parse_spans_from_file().spans[spanCounter.counter:])
+        spans = SpansContainer.parse_spans_from_file().spans
+        return SpansContainer(spans=spans[spanCounter.counter :])  # noqa
 
     def get_root(self) -> Dict[str, Any]:
-        return list(filter(lambda item: item['parent_id'] is None, self.spans))[0]
+        return list(filter(lambda item: item["parent_id"] is None, self.spans))[0]
 
     def get_children(self) -> List[Dict[str, Any]]:
-        return list(filter(lambda item: item['parent_id'] == self.get_root()['context']['span_id'], self.spans))
+        return list(
+            filter(
+                lambda item: item["parent_id"] == self.get_root()["context"]["span_id"],
+                self.spans,
+            )
+        )
 
     def get_internals(self) -> List[Dict[str, Any]]:
-        return list(filter(lambda item: item['kind'] == 'SpanKind.INTERNAL', self.get_children()))
+        return list(
+            filter(
+                lambda item: item["kind"] == "SpanKind.INTERNAL", self.get_children()
+            )
+        )
 
-    def get_non_internal_children(self, name_filter: Optional[str] = None) -> List[Dict[str, Any]]:
-        children = [item for item in self.get_children() if item not in self.get_internals()]
+    def get_non_internal_children(
+        self, name_filter: Optional[str] = None
+    ) -> List[Dict[str, Any]]:
+        children = [
+            item for item in self.get_children() if item not in self.get_internals()
+        ]
         if not name_filter:
             return children
-        return [child for child in children if child['name'] == name_filter]
+        return [child for child in children if child["name"] == name_filter]
 
     @staticmethod
-    def get_attribute_from_list_of_spans(list_of_spans: List[Dict[str, Any]], attribute_name: str) -> Any:
-        return list(filter(lambda item: item['attributes'].get(attribute_name) is not None, list_of_spans))[0]['attributes'][attribute_name]
+    def get_attribute_from_list_of_spans(
+        list_of_spans: List[Dict[str, Any]], attribute_name: str
+    ) -> Any:
+        return list(
+            filter(
+                lambda item: item["attributes"].get(attribute_name) is not None,
+                list_of_spans,
+            )
+        )[0]["attributes"][attribute_name]
