0c52916d675e49382bbf9313a5784f016ea62b89
CircleCI <no-reply@build.com>
2022-06-13 14:07:05 +0300

    add --new-version to bumpversion

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -31,7 +31,7 @@ echo "Getting latest changes from git"
 changes=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline)
 
 sudo pip install --upgrade bumpversion
-bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
+bumpversion patch --new-version --message "{current_version} → {new_version}. Changes: ${changes}"
 
 push_tags
 
