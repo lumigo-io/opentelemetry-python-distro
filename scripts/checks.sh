11209a148f059f6b0ed3985d7849d6609167802d
Michele Mancioppi <michelem@lumigo.io>
2022-07-25 21:11:57 +0200

    Update checks.sh

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-python3 -m pre-commit run -a
+pre-commit run -a
 pushd src
 LUMIGO_TRACER_TOKEN='token' py.test --cov=./lumigo_wrapper --cov-config=.coveragerc --ignore=test/integration
 popd
