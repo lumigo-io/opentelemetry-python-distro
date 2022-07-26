c9e14d91cff194be44e588ea959e77b2275002de
Michele Mancioppi <michelem@lumigo.io>
2022-07-26 18:30:16 +0200

    bug(test): Boto3 test setup fix (#57)
    By mistake we have been testing the botocore instrumentation (which works with boto3) and thought we were qualifying boto2. This change adjusts the setup so that we test and report compatibility on the right packages.

--- noxfile.py
+++ noxfile.py
@@ -140,7 +140,7 @@ def integration_tests_boto3(
     session,
     boto3_version,
 ):
-    with TestedVersions.save_tests_result("boto3", "boto3", boto3_version):
+    with TestedVersions.save_tests_result("botocore", "boto3", boto3_version):
         install_package("boto3", boto3_version, session)
 
         install(session, ".")
