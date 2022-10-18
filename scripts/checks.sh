0967b644d284ed808b869160359e3d704a166d41
CircleCI <no-reply@build.com>
2022-10-18 14:01:03 +0300

    fix

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -7,7 +7,7 @@ if [[ -n "$CI" ]]
 then
     # Check if branch contains RD ticket value.
     echo "${GITHUB_REF##*/}"
-    echo "${$GITHUB_REF_NAME##*/}"
+    echo "${GITHUB_REF_NAME##*/}"
     echo "$GITHUB_REF" | grep -E "[RDrd]-[0-9]+|master" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
 fi
 
