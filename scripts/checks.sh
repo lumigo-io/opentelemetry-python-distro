cebcb1955e58833b05201884f55c63fc987bb9c4
CircleCI <no-reply@build.com>
2022-10-18 14:20:12 +0300

    fix

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -8,7 +8,3 @@ then
     # Check if branch contains RD ticket value.
     echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
 fi
-
-pushd src
-LUMIGO_TRACER_TOKEN='token' py.test --cov=./lumigo_wrapper --cov-config=.coveragerc --ignore=test/integration --ignore=test/components
-popd
