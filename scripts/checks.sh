f3f5f80d9ddd8715b710243e176290431e6778eb
CircleCI <no-reply@build.com>
2022-10-18 12:49:24 +0300

    adding validation for lumigo branch name

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,6 +2,13 @@
 set -eo pipefail
 
 pre-commit run -a
+
+if [[ -n "$CI" ]]
+then
+    # Check if branch contains RD ticket value.
+    echo "$GITHUB_REF" | grep -E "[RDrd]-[0-9]+|master" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
+fi
+
 pushd src
 LUMIGO_TRACER_TOKEN='token' py.test --cov=./lumigo_wrapper --cov-config=.coveragerc --ignore=test/integration --ignore=test/components
 popd
