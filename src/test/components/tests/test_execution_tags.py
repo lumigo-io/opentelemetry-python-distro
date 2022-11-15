6de11aefdb1385ac649aa2272a7e26932d79ba72
shanishiri <shani@lumigo.io>
2022-11-15 15:45:49 +0200

    feat: update README.md with execution tags configuration

---
+++ src/test/components/tests/test_execution_tags.py
@@ -0,0 +1,31 @@
+import requests
+import time
+import unittest
+
+from test.test_utils.spans_parser import SpansContainer
+
+
+class TestExecutionTags(unittest.TestCase):
+    def test_execution_tag(self):
+        response = requests.get("http://localhost:8020/invoke-request")
+        response.raise_for_status()
+
+        body = response.json()
+
+        assert body is not None
+
+        # TODO Do something deterministic
+        time.sleep(3)  # Sleep for two seconds to allow the exporter to catch up
+
+        spans_container = SpansContainer.get_spans_from_file()
+        self.assertEqual(4, len(spans_container.spans))
+
+        # assert root
+        root = spans_container.get_root()
+        self.assertIsNotNone(root)
+        root_attributes = root["attributes"]
+        self.assertEquals(type(root_attributes["lumigo.execution_tags.response_len"]), int)
+        self.assertEquals(type(root_attributes["lumigo.execution_tags.response"]), str)
+        self.assertEquals(root_attributes["lumigo.execution_tags.app"], True)
+        self.assertEquals(root_attributes["lumigo.execution_tags.app.response"], "success")
+        self.assertEquals(root_attributes["lumigo.execution_tags.foo"], ["bar", "baz"])
