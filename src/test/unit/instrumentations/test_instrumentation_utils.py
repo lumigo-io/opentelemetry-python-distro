781845d2d52e8749e9b5cef9949bf1e3f69a3435
saartochner <saart@lumigo.io>
2023-08-07 13:28:50 +0300

    wip

--- src/test/unit/instrumentations/test_instrumentation_utils.py
+++ src/test/unit/instrumentations/test_instrumentation_utils.py
@@ -12,21 +12,21 @@ def test_add_body_attribute_happy_flow():
     span_mock = Mock(Span)
     add_body_attribute(span_mock, "body", "attribute")
     assert len(span_mock.set_attribute.call_args_list) == 1
-    assert span_mock.set_attribute.call_args_list[0].args == ("attribute", "body")
+    assert span_mock.set_attribute.call_args_list[0][0] == ("attribute", "body")
 
 
 def test_add_body_attribute_unicode():
     span_mock = Mock(Span)
     add_body_attribute(span_mock, "body".encode(), "attribute")
     assert len(span_mock.set_attribute.call_args_list) == 1
-    assert span_mock.set_attribute.call_args_list[0].args == ("attribute", "body")
+    assert span_mock.set_attribute.call_args_list[0][0] == ("attribute", "body")
 
 
 def test_add_body_attribute_non_utf8_binary():
     span_mock = Mock(Span)
     add_body_attribute(span_mock, b"\x80\x81\x82", "attribute")
     assert len(span_mock.set_attribute.call_args_list) == 1
-    assert span_mock.set_attribute.call_args_list[0].args == ("attribute", "0x808182")
+    assert span_mock.set_attribute.call_args_list[0][0] == ("attribute", "0x808182")
 
 
 def test_add_body_attribute_exception():
@@ -40,7 +40,7 @@ def test_add_body_attribute_too_long():
     span_mock = Mock(Span)
     add_body_attribute(span_mock, "a" * PAYLOAD_MAX_SIZE * 2, "attribute")
     assert len(span_mock.set_attribute.call_args_list) == 1
-    assert span_mock.set_attribute.call_args_list[0].args == (
+    assert span_mock.set_attribute.call_args_list[0][0] == (
         "attribute",
         "a" * PAYLOAD_MAX_SIZE,
     )
@@ -50,4 +50,4 @@ def test_add_body_attribute_non_string_or_bytes():
     span_mock = Mock(Span)
     add_body_attribute(span_mock, None, "attribute")
     assert len(span_mock.set_attribute.call_args_list) == 1
-    assert span_mock.set_attribute.call_args_list[0].args == ("attribute", "None")
+    assert span_mock.set_attribute.call_args_list[0][0] == ("attribute", "None")
