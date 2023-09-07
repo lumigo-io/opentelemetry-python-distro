40c68e81a87b833d8b262e78ce15b08cd5fb07df
Sagiv Oulu <sagivo@lumigo.io>
2023-09-07 19:42:29 +0300

    chore: auto format code

--- src/test/unit/instrumentations/botocore/test_parsers.py
+++ src/test/unit/instrumentations/botocore/test_parsers.py
@@ -6,12 +6,7 @@ from lumigo_opentelemetry.instrumentations.botocore.parsers import SqsParser
 
 EMPTY_SQS_RESULT_1 = {}
 EMPTY_SQS_RESULT_2 = {"Messages": []}
-NON_EMPTY_SQS_RESULT = {
-    "Messages": [{
-        "MessageId": "1234",
-        "Body": "test"
-    }]
-}
+NON_EMPTY_SQS_RESULT = {"Messages": [{"MessageId": "1234", "Body": "test"}]}
 
 
 @pytest.mark.parametrize(
@@ -20,10 +15,8 @@ NON_EMPTY_SQS_RESULT = {
         # Check that empty sqs polls are skipped
         ("true", "ReceiveMessage", EMPTY_SQS_RESULT_1, True),
         ("true", "ReceiveMessage", EMPTY_SQS_RESULT_2, True),
-
         # Check that non-empty polls are not skipped
         ("true", "ReceiveMessage", NON_EMPTY_SQS_RESULT, False),
-
         # Check that other operations are not skipped
         ("true", "DeleteMessage", EMPTY_SQS_RESULT_1, False),
         ("true", "DeleteMessageBatch", EMPTY_SQS_RESULT_1, False),
@@ -31,45 +24,49 @@ NON_EMPTY_SQS_RESULT = {
         ("true", "SendMessageBatch", EMPTY_SQS_RESULT_1, False),
         ("true", "UnknownOperation", EMPTY_SQS_RESULT_1, False),
         ("true", None, EMPTY_SQS_RESULT_1, False),
-
         # Check that empty sqs polls are not skipped if the env var is set to false
         ("false", "ReceiveMessage", EMPTY_SQS_RESULT_1, False),
         ("false", "ReceiveMessage", EMPTY_SQS_RESULT_2, False),
-
         # Check that non-empty polls are not skipped if the env var is set to false
         ("false", "ReceiveMessage", NON_EMPTY_SQS_RESULT, False),
-
         # Check that the default behavior is to skip empty sqs polls
         (None, "ReceiveMessage", EMPTY_SQS_RESULT_1, True),
         (None, "ReceiveMessage", EMPTY_SQS_RESULT_2, True),
         ("UnsupportedEnvVarValue", "ReceiveMessage", EMPTY_SQS_RESULT_2, True),
-    ]
+    ],
 )
-def test_sqs_skip_sqs_response(env_var_value, operation, result, should_skip, monkeypatch):
+def test_sqs_skip_sqs_response(
+    env_var_value, operation, result, should_skip, monkeypatch
+):
     if env_var_value is not None:
         monkeypatch.setenv("AUTO_FILTER_EMPTY_SQS", env_var_value)
 
-    assert SqsParser._should_skip_empty_sqs_polling_response(operation, result) == should_skip
+    assert (
+        SqsParser._should_skip_empty_sqs_polling_response(operation, result)
+        == should_skip
+    )
 
 
-@patch("lumigo_opentelemetry.instrumentations.botocore.parsers.SqsParser._should_skip_empty_sqs_polling_response")
+@patch(
+    "lumigo_opentelemetry.instrumentations.botocore.parsers.SqsParser._should_skip_empty_sqs_polling_response"
+)
 def test_parse_sqs_response_skipping_empty_polls_outputs_log(should_skip_mock, caplog):
     should_skip_mock.return_value = True
     span = Mock(set_attribute=Mock())
-    service_name = 'sqs'
-    operation_name = 'ReceiveMessage'
+    service_name = "sqs"
+    operation_name = "ReceiveMessage"
     result = {
-        'ResponseMetadata': {
-            'RequestId': '54bf0dab-cfab-5fa5-b284-50d83403c94c',
-            'HTTPStatusCode': 200,
-            'HTTPHeaders': {
-                'x-amzn-requestid': '54bf0dab-cfab-5fa5-b284-50d83403c94c',
-                'date': 'Thu, 07 Sep 2023 16:25:12 GMT',
-                'content-type': 'text/xml',
-                'content-length': '240',
-                'connection': 'keep-alive'
+        "ResponseMetadata": {
+            "RequestId": "54bf0dab-cfab-5fa5-b284-50d83403c94c",
+            "HTTPStatusCode": 200,
+            "HTTPHeaders": {
+                "x-amzn-requestid": "54bf0dab-cfab-5fa5-b284-50d83403c94c",
+                "date": "Thu, 07 Sep 2023 16:25:12 GMT",
+                "content-type": "text/xml",
+                "content-length": "240",
+                "connection": "keep-alive",
             },
-            'RetryAttempts': 0
+            "RetryAttempts": 0,
         }
     }
 
@@ -78,24 +75,26 @@ def test_parse_sqs_response_skipping_empty_polls_outputs_log(should_skip_mock, c
     assert "not tracing empty sqs polling requests" in caplog.text.lower()
 
 
-@patch("lumigo_opentelemetry.instrumentations.botocore.parsers.SqsParser._should_skip_empty_sqs_polling_response")
+@patch(
+    "lumigo_opentelemetry.instrumentations.botocore.parsers.SqsParser._should_skip_empty_sqs_polling_response"
+)
 def test_parse_sqs_response_not_skipping_polls_no_output_log(should_skip_mock, caplog):
     should_skip_mock.return_value = False
     span = Mock(set_attribute=Mock())
-    service_name = 'sqs'
-    operation_name = 'ReceiveMessage'
+    service_name = "sqs"
+    operation_name = "ReceiveMessage"
     result = {
-        'ResponseMetadata': {
-            'RequestId': '54bf0dab-cfab-5fa5-b284-50d83403c94c',
-            'HTTPStatusCode': 200,
-            'HTTPHeaders': {
-                'x-amzn-requestid': '54bf0dab-cfab-5fa5-b284-50d83403c94c',
-                'date': 'Thu, 07 Sep 2023 16:25:12 GMT',
-                'content-type': 'text/xml',
-                'content-length': '240',
-                'connection': 'keep-alive'
+        "ResponseMetadata": {
+            "RequestId": "54bf0dab-cfab-5fa5-b284-50d83403c94c",
+            "HTTPStatusCode": 200,
+            "HTTPHeaders": {
+                "x-amzn-requestid": "54bf0dab-cfab-5fa5-b284-50d83403c94c",
+                "date": "Thu, 07 Sep 2023 16:25:12 GMT",
+                "content-type": "text/xml",
+                "content-length": "240",
+                "connection": "keep-alive",
             },
-            'RetryAttempts': 0
+            "RetryAttempts": 0,
         }
     }
 
