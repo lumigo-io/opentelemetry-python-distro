efda5e941a9630df5061e3630ca2f20b9d1e60e2
Sagiv Oulu <sagivo@lumigo.io>
2023-09-10 10:17:15 +0300

    fix: wrong attribute name used

--- src/lumigo_opentelemetry/resources/span_processor.py
+++ src/lumigo_opentelemetry/resources/span_processor.py
@@ -6,7 +6,7 @@ from lumigo_opentelemetry import logger
 
 
 # A span attributes that if is set to True, the span will not be exported
-SKIP_EXPORT_SPAN_ATTRIBUTE = "NO_EXPORT"
+SKIP_EXPORT_SPAN_ATTRIBUTE = "SKIP_EXPORT"
 
 
 class LumigoSpanProcessor(BatchSpanProcessor):
