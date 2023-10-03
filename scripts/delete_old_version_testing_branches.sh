e61ded4f2881c6c1472a190c84fb2383f2ecaab1
Adam Fisher <adamf@lumigo.io>
2023-10-03 18:48:30 +0300

    test: bashisms

--- scripts/delete_old_version_testing_branches.sh
+++ scripts/delete_old_version_testing_branches.sh
@@ -6,7 +6,16 @@ today=$(date +%Y%m%d)
 echo "today: $today"
 git fetch --unshallow --force
 echo "gathering version testing branches..."
-version_testing_branches=$(git branch -r | grep -E '(version-testing-[0-9]{8})' | grep -v "$today" | awk -F'/' '{print $2}')
+version_testing_branches=$(\
+    git branch -r \
+    | grep -E '(version-testing-[0-9]{8})' || echo "" \
+    | grep -v "$today" || echo "" \
+    | awk -F'/' '{print $2}'\
+)
+if [ -z "$version_testing_branches" ]; then
+    echo "No version testing branches need to be removed."
+    exit 0
+fi
 echo "branches to be removed:"
 echo "$version_testing_branches"
 echo "removing branches..."
