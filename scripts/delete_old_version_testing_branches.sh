47457253f5819c397aa456f90372cdf4ea8502a6
Adam Fisher <adamf@lumigo.io>
2023-10-03 18:48:30 +0300

    fix: version branch deletion script

--- scripts/delete_old_version_testing_branches.sh
+++ scripts/delete_old_version_testing_branches.sh
@@ -4,8 +4,10 @@ set -eo pipefail
 
 today=$(date +%Y%m%d)
 echo "today: $today"
-git fetch --prune
+git fetch --unshallow --force
+echo "gathering version testing branches..."
 version_testing_branches=$(git branch -r | grep -E '(version-testing-[0-9]{8})' | grep -v "$today" | awk -F'/' '{print $2}')
 echo "branches to be removed:"
 echo "$version_testing_branches"
+echo "removing branches..."
 echo "$version_testing_branches" | xargs -I {} git push origin --delete {}
