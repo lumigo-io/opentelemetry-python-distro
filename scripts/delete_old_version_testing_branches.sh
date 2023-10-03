3eb19e3539b417f9e300ab0d15b5a4858bd4fc0b
Adam Fisher <adamf@lumigo.io>
2023-10-03 18:48:30 +0300

    fix: branch cleanup and PR description

--- scripts/delete_old_version_testing_branches.sh
+++ scripts/delete_old_version_testing_branches.sh
@@ -3,20 +3,29 @@
 set -eo pipefail
 
 today=$(date +%Y%m%d)
-echo "today: $today"
-git fetch --unshallow --force
-echo "gathering version testing branches..."
-version_testing_branches=$(\
-    git branch -r \
-    | grep -E '(version-testing-[0-9]{8})' || echo "" \
-    | grep -v "$today" || echo "" \
-    | awk -F'/' '{print $2}'\
-)
+echo "Today: $today"
+if [ "$CI" = true ]; then
+    unshallow_flag="--unshallow"
+fi
+git fetch $unshallow_flag --force
+echo "Gathering version testing branches..."
+version_testing_branches=$(git branch -r | grep -E '(version-testing-[0-9]{8})' || echo "")
+if [ -z "$version_testing_branches" ]; then
+    echo "No version testing branches found."
+    exit 0
+fi
+today_branches=$(echo "$version_testing_branches" | grep "$today" || echo "")
+if [ -z "$today_branches" ]; then
+    echo "No version testing branch from today needs to be protected."
+else
+    echo "Removing today's version testing branch from the list of branches to be removed..."
+    branches_for_deletion=$(echo "$version_testing_branches" | grep -v "$today" | awk -F'/' '{print $2}')
+fi
 if [ -z "$version_testing_branches" ]; then
     echo "No version testing branches need to be removed."
     exit 0
 fi
-echo "branches to be removed:"
-echo "$version_testing_branches"
-echo "removing branches..."
-echo "$version_testing_branches" | xargs -I {} git push origin --delete {}
+echo "Branches marked for removal:"
+echo "$branches_for_deletion"
+echo "Removing branches..."
+echo "$branches_for_deletion" | xargs -I {} git push origin --delete {}
