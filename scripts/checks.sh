0a6b15121de121ed269ba2a235259bacacc0ab0b
Eugene Orlovsky <eugeneo@lumigo.io>
2024-09-17 15:44:36 +0200

    TRAC-22-Fix-python-otel-distro-versions-table (#885)
    * actions/download-artifact@v3
    README.md
    
    * $CIRCLE_BRANCH updates,
    
    ---------
    
    Co-authored-by: Eugene Orlovsky <eugene.orlovsky@staircase.co>

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -14,7 +14,7 @@ popd
 if [[ -n "$CIRCLECI" ]]
 then
     # Check if branch contains RD ticket value.
-    echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master|version-testing-[0-9]{4}[0-9]{2}[0-9]{2}|dependabot/" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
+    echo "$CIRCLE_BRANCH" | grep -E "[TRACtrac]-[0-9]+|[RDrd]-[0-9]+|master|version-testing-[0-9]{4}[0-9]{2}[0-9]{2}|dependabot/" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
 fi
 
 # update the README's package compatibility matrix
