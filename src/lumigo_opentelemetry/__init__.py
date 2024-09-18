d0e763de522f0700ee85ab6245b996beb26141cd
Eugene Orlovsky <eugeneo@lumigo.io>
2024-09-18 09:47:49 +0200

    TRAC-1-Support-python-3.12-in-python-otel-distro (#873)
    adding python support 3.12
    
    ---------
    
    Co-authored-by: Eugene Orlovsky <eugene.orlovsky@staircase.co>

--- src/lumigo_opentelemetry/__init__.py
+++ src/lumigo_opentelemetry/__init__.py
@@ -81,11 +81,11 @@ def init() -> Dict[str, Any]:
 
     try:
         python_version = sys.version_info
-        # Check if the major version is 3 and the minor version is between 8 and 11
-        if python_version.major != 3 or not (8 <= python_version.minor <= 11):
+        # Check if the major version is 3 and the minor version is between 8 and 12
+        if python_version.major != 3 or not (8 <= python_version.minor <= 12):
             logger.warning(
                 f"Unsupported Python version {python_version.major}.{python_version.minor}; "
-                "only Python 3.8 to 3.11 are supported."
+                "only Python 3.8 to 3.12 are supported."
             )
             return {}
 
