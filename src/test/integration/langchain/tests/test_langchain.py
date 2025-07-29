741ca73e0f8cda2e4f96d59c0770a387fc7993e8
Moshe Shaham <moshes@lumigo.io>
2025-07-29 10:44:17 +0300

    RD-16267-Add-OpenLLMetry-auto-instrumentation

--- src/test/integration/langchain/tests/test_langchain.py
+++ src/test/integration/langchain/tests/test_langchain.py
@@ -7,16 +7,17 @@ import subprocess
 
 
 
-class TestFlaskSpans(unittest.TestCase):
-    sample_path = path.join(
-        path.dirname(path.abspath(__file__)),
-        f"../app/app.py",
-    )
-    subprocess.check_output(
-        [sys.executable, sample_path],
-        env={
-            **os.environ,
-            "AUTOWRAPT_BOOTSTRAP": "lumigo_opentelemetry",
-            "OTEL_SERVICE_NAME": f"langchain-app",
-        },
-    )
+class TestLangchainSpans(unittest.TestCase):
+    def test_langchain_spans(self):
+        sample_path = path.join(
+            path.dirname(path.abspath(__file__)),
+            f"../app/app.py",
+        )
+        subprocess.check_output(
+            [sys.executable, sample_path],
+            env={
+                **os.environ,
+                "AUTOWRAPT_BOOTSTRAP": "lumigo_opentelemetry",
+                "OTEL_SERVICE_NAME": f"langchain-app",
+            },
+        )
